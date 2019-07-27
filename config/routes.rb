a6c57635bee5af5a04698dd0464a0538623e1aa1
Ryan Miller <ryanmillergm@gmail.com>
2019-07-27 11:24:05 -0600

    Adds forecast test and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      namespace :forecast do
+        get '/forecast', to: 'forecast#index'
+      end
+
+      resources :forecast, only: [:index]
+    end
+  end
 end
---
+++ spec/requests/forecast_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'Forecasts' do
+  it 'gets latitude and longitude from geocoding' do
+
+    get '/api/v1/forecast?location=denver,co'
+
+    expect(response).to be(successful)
+
+    expect(response).to eq(39.739235, -104.990250)
+  end
+end
