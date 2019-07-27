515356106c58c4617343a807e2026129ea0c2c12
Ryan Miller <ryanmillergm@gmail.com>
2019-07-27 11:25:35 -0600

    Adds and fixed route

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      namespace :forecast do
-        get '/forecast', to: 'forecast#index'
-      end
-
       resources :forecast, only: [:index]
     end
   end
