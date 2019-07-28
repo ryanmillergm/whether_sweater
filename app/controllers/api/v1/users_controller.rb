431d61ac9b27a232405849b28b7bc0eced9ac428
Ryan Miller <ryanmillergm@gmail.com>
2019-07-28 15:56:27 -0600

    Adds api_key to user and renders json back

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,22 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
+  def index
+    # render json: UsersSerializer(User.new)
+  end
+
+  def create
+    user = User.new(user_params)
+    user.save
+    if user.save
+      user.api_key = SecureRandom.urlsafe_base64.to_s
+      render json: {api_key: user.api_key}, status: :created
+    elsif
+      render nothing: true, status: :bad_request
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
 end
--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -3,11 +3,23 @@ require 'rails_helper'
 RSpec.describe 'Users' do
   it 'can create an account' do
 
-    get '/api/v1/users'
+    body = {
+      email: "whatever@example.com",
+      password: "password",
+      password_confirmation: "password"
+    }
+
+    post '/api/v1/users', params: body
 
     expect(response).to be_successful
 
+    api_key = JSON.parse(response.body, symbolize_names: true)
+
+    expected = {
+        api_key: "jgn983hy48thw9begh98h4539h4",
+      }
+
     expect(response).to have_http_status(201)
-    binding.pry
+    expect(api_key).to have_key(:api_key)
   end
 end
