73a4c0426a9023e532b0ff41b08164a85af8edc6
Ryan Miller <ryanmillergm@gmail.com>
2019-07-28 10:39:13 -0600

    Adds user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "validations" do
-    it { should validates_presence_of :email }
-    it { should validates_presence_of :password }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :password }
   end
 end
