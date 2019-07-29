3104c65a7f4c7462b4e902412cd551e5dee14e66
Ryan Miller <ryanmillergm@gmail.com>
2019-07-29 10:41:01 -0600

    refactoring to google services

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,23 +1,32 @@
 class Api::V1::MunchiesController < ApplicationController
   def index
-
-    google_response = Faraday.get("https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=AIzaSyDXJ9V7QreVlZEn-EPF4yMOrUCFrv4xdow")
-
-    travel_time = JSON.parse(google_response.body, symbolize_names: true)[:routes][0][:legs][0][:duration][:text].split.first.to_i
-    travel_time_interval_type = JSON.parse(google_response.body, symbolize_names: true)[:routes][0][:legs][0][:duration][:text].split[1]
+    render json: MunchiesSerializer.new(RestaurantFinderFacade.new(munchies_params))
 
 
-    params[:start]
-    params[:end]
-    params[:food]
+    # google_response = Faraday.get("https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=AIzaSyDXJ9V7QreVlZEn-EPF4yMOrUCFrv4xdow")
+    #https://maps.googleapis.com/maps/api/directions/json?origin=denver,co&destination=pueblo,co&key=AIzaSyDXJ9V7QreVlZEn-EPF4yMOrUCFrv4xdow
+    # travel_time = JSON.parse(google_response.body, symbolize_names: true)[:routes][0][:legs][0][:duration][:text].split.first.to_i
+    # travel_time_interval_type = JSON.parse(google_response.body, symbolize_names: true)[:routes][0][:legs][0][:duration][:text].split[1]
+    #
+    #
+    # params[:start]
+    # params[:end]
+    # params[:food]
+    #
+    # yelp_response = Faraday.get("https://api.yelp.com/v3/businesses/search?location=pueblo,co&term=food&categories=chinese&open_at=1564419210&imit=3") do |f|
+    #   f.headers = { Authorization: "Bearer #{ENV["YELP_API_KEY"]}" }
+    #   end
+    # results = JSON.parse(yelp_response.body, symbolize_names: true)[:businesses]
+    #
+    # @restaurants = results.each do |result|
+    #   Restaurant.new(result)
+    # end
+    binding.pry
+  end
 
-    yelp_response = Faraday.get("https://api.yelp.com/v3/businesses/search?location=pueblo,co&term=food&categories=chinese&open_at=1564419210&imit=3") do |f|
-      f.headers = { Authorization: "Bearer #{ENV["YELP_API_KEY"]}" }
-      end
-    results = JSON.parse(yelp_response.body, symbolize_names: true)[:businesses]
+  private
 
-    @restaurants = results.each do |result|
-      Restaurant.new(result)
-    end
+  def munchies_params
+    params.permit(:start, :end, :food)
   end
 end
--- app/facades/restaurant_finder_facade.rb
+++ app/facades/restaurant_finder_facade.rb
@@ -1,5 +1,17 @@
 class RestaurantFinderFacade
-  def initialize(argument)
-    @argument = argument
+  def initialize(attributes = {})
+    binding.pry
+    @start = attributes[:start]
+    @end = attributes[:end]
+    @food = attributes[:food]
+    @restaurants = something
+  end
+
+  def get_google_trave_time
+    travil_time = google_directions_service.get_time
+  end
+
+  def private goodle_directions_service
+    @google_directions ||= GoogleDirectionsService.new()
   end
 end
