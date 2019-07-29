59f947ba500817f2158ad60558dfc5b2b03c2f69
Ryan Miller <ryanmillergm@gmail.com>
2019-07-29 09:55:22 -0600

    Set travel time and interval type in munchies controller

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,10 +1,13 @@
 class Api::V1::MunchiesController < ApplicationController
   def index
 
-    response = Faraday.get("https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=AIzaSyDXJ9V7QreVlZEn-EPF4yMOrUCFrv4xdow")
+    google_response = Faraday.get("https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=AIzaSyDXJ9V7QreVlZEn-EPF4yMOrUCFrv4xdow")
 
+    travel_time = JSON.parse(google_response.body, symbolize_names: true)[:routes][0][:legs][0][:duration][:text].split.first.to_i
+    travel_time_interval_type = JSON.parse(google_response.body, symbolize_names: true)[:routes][0][:legs][0][:duration][:text].split[1]
+
+    yelp_response = Faraday.get()
     binding.pry
-    JSON.parse(response.body, symbolize_names: true)
     params[:start]
     params[:end]
     params[:food]
