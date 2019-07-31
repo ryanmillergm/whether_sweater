9d3262a42dfdf2d170927a071dd698bc88c1331e
Ryan Miller <ryanmillergm@gmail.com>
2019-07-31 12:59:28 -0600

    Adds hourly forecast model test

--- app/models/hourly_forecast.rb
+++ app/models/hourly_forecast.rb
@@ -1,4 +1,8 @@
 class HourlyForecast
+  attr_reader :hour,
+              :icon,
+              :temperature
+              
   def initialize(forecast)
     @hour = Time.at(forecast["time"]).hour
     @icon = forecast["icon"]
