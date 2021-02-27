e80504b9b8c66ecfe6300b0795447ed82b251b5d
Ryan Miller <ryanmillergm@gmail.com>
2021-02-27 16:12:33 -0700

    Updates user json response

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,13 +2,13 @@ class Api::V1::UsersController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :create
 
   def create
-    user = User.new(user_params)
-    if user.save
+    @user = User.new(user_params)
+    if @user.save
       api_key = SecureRandom.urlsafe_base64.to_s
-      user.update(api_key: api_key)
-      render json: {api_key: user.api_key}, status: :created
+      @user.update(api_key: api_key)
+      render_response
     else
-      render json: { errors: user.errors.full_messages }, status: 401
+      render json: { errors: @user.errors.full_messages }, status: 401
     end
   end
 
@@ -16,5 +16,18 @@ class Api::V1::UsersController < ApplicationController
 
   def user_params
     params.permit(:email, :password, :password_confirmation)
+  end
+
+  def render_response
+    render json: {
+      data: {
+        type: @user.class.name,
+        id: @user.id,
+        attributes: {
+          email: @user.email,
+          api_key: @user.api_key
+        }
+      }
+    }, status: :created
   end
 end
